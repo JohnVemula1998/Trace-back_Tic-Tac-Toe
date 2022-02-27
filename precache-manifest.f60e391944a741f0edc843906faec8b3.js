57e47462e8b1d144c2672b0ac343613c044de827
JohnVemula1998 <johnvemula7771998@gmail.com>
2022-02-27 18:28:49 +0530

    Updates

---
+++ precache-manifest.f60e391944a741f0edc843906faec8b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc2b0a3ab2af7963eae2",
+    "url": "/Trace-back_Tic-Tac-Toe/static/css/main.36041a1d.chunk.css"
+  },
+  {
+    "revision": "dc2b0a3ab2af7963eae2",
+    "url": "/Trace-back_Tic-Tac-Toe/static/js/main.44e89a5e.chunk.js"
+  },
+  {
+    "revision": "2daee2591f40b7da98e7",
+    "url": "/Trace-back_Tic-Tac-Toe/static/js/runtime~main.13c96206.js"
+  },
+  {
+    "revision": "a15dd23a3f38431d7a2d",
+    "url": "/Trace-back_Tic-Tac-Toe/static/js/2.0a12f12c.chunk.js"
+  },
+  {
+    "revision": "7e55f5a120f96f1f258e43d5d91f6ac8",
+    "url": "/Trace-back_Tic-Tac-Toe/index.html"
+  }
+];
